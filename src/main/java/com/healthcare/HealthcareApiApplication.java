fa13774ee57449d77d9f19d76a3d879d3404e41b
Muhammad Fayzan Siddiqui <fayzan.siddiqui@11street.my>
2018-07-22 03:00:09 +0800

    initial app

---
+++ src/main/java/com/healthcare/HealthcareApiApplication.java
@@ -0,0 +1,12 @@
+package com.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareApiApplication.class, args);
+	}
+}
